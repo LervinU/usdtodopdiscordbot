896b90b03c66757f3733f4f365e8cc15d45515d6
Lervin <lervin100@gmail.com>
2021-05-10 13:18:51 -0400

    API integration

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const scrape =  require('./src/scrape');
+const fetch = require('node-fetch');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -11,38 +12,61 @@ client.on('ready', () => {
 
 client.on('message', msg =>  {
 
-  if(msg.content === "!dollar-help") {
+  if(msg.content === "!Banks") {
     msg.reply(
       `
       Commands:
-      Help: !dollar-help
-      Banreservas: !Banreservas
-      ScotiaBank: !Scotiabank
+      Help: !Banks
+      **Banreservas**: !Banreservas
+      **ScotiaBank**: !Scotiabank
+      **BHD**: !BHD
+      **Popular**: !Popular
+      **Banco Caribe**: !BancoCaribe
       `
     )
   }
 
   if (msg.content === '!Banreservas') {
-    const scrappedData = Promise.resolve(scrape.scrapeBanresData);
-    scrappedData.then((data) => {
-        msg.reply(`**Banreservas** Compra: **${data.buys}** Venta: **${data.sells}**`);
-    });
+    fetch(`${process.env.DOLLAR_API_URL}/Banreservas`)
+      .then(res => res.json())
+      .then(data => {
+        msg.reply(`**Banreservas** Compra: **${data.buysDollar}** Venta: **${data.sellsDollar}**`)
+      });
   }
 
   if(msg.content === "!Scotiabank") {
-    const scrappedData = Promise.resolve(scrape.scrapeScotiaBankData);
-    scrappedData.then((data) => {
-      msg.reply(`**Scotiabank** Compra: **${data.buys}** Venta: **${data.sells}**`);
+    fetch(`${process.env.DOLLAR_API_URL}/Scotiabank`)
+    .then(res => res.json())
+    .then(data => {
+      msg.reply(`**Scotiabank** Compra: **${data.buysDollar}** Venta: **${data.sellsDollar}**`)
     });
   }
 
   if(msg.content === "!Popular") {
-    const scrappedData = Promise.resolve(scrape.scrapePopularData);
-    scrappedData.then((data) => {
-      msg.reply(`**Banco Popular Dominicano** -  In development`);
+    fetch(`${process.env.DOLLAR_API_URL}/Popular`)
+    .then(res => res.json())
+    .then(data => {
+      msg.reply(`**Banco Popular** Compra: **${data.buysDollar}** Venta: **${data.sellsDollar}**`)
+    });
+  }
+
+  if(msg.content === "!BHD") {
+    fetch(`${process.env.DOLLAR_API_URL}/BHD`)
+    .then(res => res.json())
+    .then(data => {
+      msg.reply(`**BHD Leon** Compra: **${data.buysDollar}** Venta: **${data.sellsDollar}**`)
+    });
+  }
+  if(msg.content === "!BancoCaribe") {
+    fetch(`${process.env.DOLLAR_API_URL}/Bancocaribe`)
+    .then(res => res.json())
+    .then(data => {
+      msg.reply(`**Banco Caribe** Compra: **${data.buysDollar}** Venta: **${data.sellsDollar}**`)
     });
   }
 });
+
+
 
 
 
